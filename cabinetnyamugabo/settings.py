c3dc10d5f6d5abea5a62e2f6d4dab4b33335fc2d
john591 <johnkalume0@gmail.com>
2026-04-29 19:58:02 +0100

    Prepare backend and database for Render deployment

--- cabinetnyamugabo/settings.py
+++ cabinetnyamugabo/settings.py
@@ -229,10 +229,6 @@ CORS_ALLOWED_ORIGINS = get_env_list(
     [
         "http://127.0.0.1:3000",
         "http://localhost:3000",
-        "https://cabinetnyamugabo.onrender.com",
-        "https://www.cabinetnyamugabo.com",
-        "https://cabinetnyamugabo.com",
-        "https://www.cabinetnyamugabo.onrender.com",
         "https://cabinenyamugabo.vercel.app",
 
     ],
@@ -243,10 +239,6 @@ CSRF_TRUSTED_ORIGINS = get_env_list(
     [
         "http://127.0.0.1:3000",
         "http://localhost:3000",
-        "https://cabinetnyamugabo.onrender.com",
-        "https://www.cabinetnyamugabo.com",
-        "https://cabinetnyamugabo.com",
-        "https://www.cabinetnyamugabo.onrender.com",
         "https://cabinenyamugabo.vercel.app",
     ],
 )
